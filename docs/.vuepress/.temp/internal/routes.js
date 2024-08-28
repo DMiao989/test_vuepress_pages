bc00f0c78f7d95781475904af47cd99f88c66b3e
Ding Miao <dingmiao@hollysys.com>
2024-08-28 09:37:04 +0800

    init project

---
+++ docs/.vuepress/.temp/internal/routes.js
@@ -0,0 +1,6 @@
+export const redirects = JSON.parse("{}")
+
+export const routes = Object.fromEntries([
+  ["/", { loader: () => import(/* webpackChunkName: "index.html" */"/Users/dingmiao/Desktop/vuepress-starter/docs/.vuepress/.temp/pages/index.html.js"), meta: {"title":"Hello, VuePress!"} }],
+  ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"/Users/dingmiao/Desktop/vuepress-starter/docs/.vuepress/.temp/pages/404.html.js"), meta: {"title":""} }],
+]);
---
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -0,0 +1,3 @@
+import comp from "/Users/dingmiao/Desktop/vuepress-starter/docs/.vuepress/.temp/pages/index.html.vue"
+const data = JSON.parse("{\"path\":\"/\",\"title\":\"Hello, VuePress!\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[],\"git\":{},\"filePathRelative\":\"README.md\"}")
+export { comp, data }
