4ffeb72fa3fa9ebf7a45ef90bc0e9a1593b00f15
Ding Miao <dingmiao@hollysys.com>
2024-08-28 10:17:37 +0800

    test1

--- docs/.vuepress/.temp/pages/index.html.js
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -1,3 +1,3 @@
 import comp from "/Users/dingmiao/Desktop/vuepress-starter/docs/.vuepress/.temp/pages/index.html.vue"
-const data = JSON.parse("{\"path\":\"/\",\"title\":\"Hello, VuePress!\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[],\"git\":{},\"filePathRelative\":\"README.md\"}")
+const data = JSON.parse("{\"path\":\"/\",\"title\":\"Hello, VuePress!\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[],\"git\":{\"updatedTime\":1724809024000,\"contributors\":[{\"name\":\"Ding Miao\",\"email\":\"dingmiao@hollysys.com\",\"commits\":1}]},\"filePathRelative\":\"README.md\"}")
 export { comp, data }
