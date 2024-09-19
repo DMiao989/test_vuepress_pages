b4011a9c36e2e3efbd9107fc89d71a7dbc6f2197
Ding Miao <dingmiao@hollysys.com>
2024-09-19 16:10:26 +0800

    second commit

--- docs/.vuepress/.temp/internal/siteData.js
+++ docs/.vuepress/.temp/internal/siteData.js
@@ -1,1 +1,14 @@
 export const siteData = JSON.parse("{\"base\":\"/test_vuepress_pages/\",\"lang\":\"en-US\",\"title\":\"丁苗的博客\",\"description\":\"个人学习网站，基于vuePress+gitHub_pages\",\"head\":[],\"locales\":{\"/\":{\"lang\":\"zh-CN\"}}}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updateSiteData) {
+    __VUE_HMR_RUNTIME__.updateSiteData(siteData)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ siteData }) => {
+    __VUE_HMR_RUNTIME__.updateSiteData(siteData)
+  })
+}
--- docs/.vuepress/.temp/pages/learn_vue/learn_vuex.html.js
+++ docs/.vuepress/.temp/pages/learn_vue/learn_vuex.html.js
@@ -1,3 +1,16 @@
 import comp from "/Users/dingmiao/Desktop/vuepress-starter/docs/.vuepress/.temp/pages/learn_vue/learn_vuex.html.vue"
 const data = JSON.parse("{\"path\":\"/learn_vue/learn_vuex.html\",\"title\":\"vuex中存什么？\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"headers\":[],\"git\":{\"updatedTime\":1726724335000,\"contributors\":[{\"name\":\"Ding Miao\",\"email\":\"dingmiao@hollysys.com\",\"commits\":1}]},\"filePathRelative\":\"learn_vue/learn_vuex.md\"}")
 export { comp, data }
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
